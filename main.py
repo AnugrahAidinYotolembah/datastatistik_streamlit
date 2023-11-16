759d8f67ba4ddedb43debf06cd1ffde9aba09290
Anugrah Aidin Yotolembah <108518030+AnugrahAidinYotolembah@users.noreply.github.com>
2023-11-17 02:22:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
 
-# Data dummy untuk tujuan demonstrasi
-data = pd.DataFrame({
-    'Category': ['A', 'B', 'C', 'D', 'E'],
-    'Value': [4, 7, 1, 8, 5]
-})
+# Load dataset
+df_day = pd.read_csv('dataset/day.csv')
+df_hour = pd.read_csv('dataset/hour.csv')
 
-# Judul aplikasi
-st.title('Dashboard Statistik')
+# Drop unnecessary columns
+df_day.drop(['dteday','instant','season','yr','mnth','weekday','workingday','weathersit','temp','atemp','hum', 'windspeed', 'casual', 'registered'], axis=1, inplace=True)
+df_hour.drop(['instant','dteday','season','yr','mnth','hr','holiday','weekday','workingday','temp','atemp','hum','windspeed','casual','registered'], axis=1, inplace=True)
 
-# Tampilkan data statistik
-st.header('Data Statistik')
-st.dataframe(data)
+# Sidebar
+st.sidebar.title("Proyek Analisis Data - Hour.csv dan Day.csv")
+st.sidebar.write("Nama: Moh. Abied Rabbani")
+st.sidebar.write("Email: abiedrabbani90@gmail.com")
+st.sidebar.write("Id Dicoding: Moh. Abied Rabbani")
 
-# Tampilkan ringkasan statistik
-st.subheader('Ringkasan Statistik')
-st.write(data.describe())
+# Data Wrangling
+st.title("Data Wrangling")
 
-# Tampilkan diagram batang
-st.subheader('Diagram Batang')
-st.bar_chart(data.set_index('Category')['Value'])
+# Show DataFrames
+st.header("Dataframe Hari")
+st.write(df_day)
 
-# Tampilkan diagram lingkaran
-st.subheader('Diagram Lingkaran')
-st.pie_chart(data.set_index('Category')['Value'])
+st.header("Dataframe Jam")
+st.write(df_hour)
 
-# Tampilkan histogram
-st.subheader('Histogram')
-st.hist(data['Value'], bins=10, edgecolor='black')
+# Exploratory Data Analysis (EDA)
+st.title("Exploratory Data Analysis (EDA)")
 
-# Tampilkan scatter plot
-st.subheader('Scatter Plot')
-st.scatter_chart(data)
+# Jumlah Penyewaan Sepeda pada Hari Libur dan Hari Kerja
+st.header("Jumlah Penyewaan Sepeda pada Hari Libur dan Hari Kerja")
+rentals_per_day_type = df_day.groupby('holiday')['cnt'].sum()
+st.bar_chart(rentals_per_day_type)
 
-# Tampilkan statistik tambahan
-st.subheader('Statistik Tambahan')
-st.write(f"Jumlah data: {len(data)}")
-st.write(f"Rata-rata nilai: {np.mean(data['Value'])}")
+# Korelasi antara Weathersit dan Jumlah Penyewaan Sepeda
+st.header("Korelasi antara Weathersit dan Jumlah Penyewaan Sepeda")
+corr_numerical = df_hour.corr('pearson')[['weathersit','cnt']].sort_values(by='weathersit', ascending=False)
 
-# Footer
-st.sidebar.markdown('Dibuat oleh [didi]')
+# Menampilkan heatmap dengan pyplot
+plt.figure(figsize=(8, 5))
+sns.heatmap(corr_numerical, annot=True, cmap='coolwarm', fmt=".2f")
+st.pyplot()
+
+# Conclusion
+st.title("Conclusion")
+st.write("- Jumlah penyewaan sepeda pada hari kerja lebih banyak dari pada hari libur.")
+st.write("- Hasil korelasi menunjukkan adanya hubungan yang signifikan antara kondisi cuaca dan jumlah sepeda yang disewa.")
